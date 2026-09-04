7b64209374502b41fbad2886ce0f888a0ca8ccc4
Pooja-Dhakad <poojadhakad74899@gmail.com>
2026-09-04 05:24:30 -0700

    delete account add

--- routes/user/userRoutes.js
+++ routes/user/userRoutes.js
@@ -1,8 +1,27 @@
 import express from "express";
 import { saveUser } from "../../controllers/user/userController.js";
+import {
+  sendDeleteOtp,
+  verifyDeleteOtp,
+  cancelDeletion,
+} from "../../controllers/user/deleteAccountController.js";
 
 const router = express.Router();
 
 router.post("/api/user/save", saveUser);
+router.post(
+  "/api/user/delete/send-otp",
+  sendDeleteOtp
+);
+
+router.post(
+  "/api/user/delete/verify-otp",
+  verifyDeleteOtp
+);
+
+router.post(
+  "/api/user/delete/cancel",
+  cancelDeletion
+);
 
 export default router;
